074b93bcd57284c36cd839157ead492b4e03ff87
gustavorodr <gustavorodrigues.txt@gmail.com>
2021-07-06 22:15:52 -0300

    criaçao das rotas para instiuiçao

---
+++ app/controllers/api/v1/instituicaos_controller.rb
@@ -0,0 +1,45 @@
+module Api
+	module V1
+		class InstituicaosController < ApplicationController   
+      # Listar todos as instituicões
+			def index
+				instituicaos = Instituicao.order('created_at DESC');
+				render json: {status: 'SUCCESS', message:'Instituicoes carregadas', data:instituicaos},status: :ok
+			end
+			# Listar instituicão passando ID
+			def show
+				instituicao = Instituicao.find(params[:id])
+				render json: {status: 'SUCCESS', message:'Instituicao carregada', data:instituicao},status: :ok
+			end
+			# Criar um novo artigo
+			def create
+				instituicao = Instituicao.new(instituicao_params)
+				if instituicao.save
+					render json: {status: 'SUCCESS', message:'Instituicao salva', data:instituicao},status: :ok
+				else
+					render json: {status: 'ERROR', message:'Instituicao nao salva', data:instituicao.erros},status: :unprocessable_entity
+				end
+			end
+			# Excluir artigo
+			def destroy
+				instituicao = Instituicao.find(params[:id])
+				instituicao.destroy
+				render json: {status: 'SUCCESS', message:'Instituicao excluida', data:instituicao},status: :ok
+			end
+			# Atualizar um artigo
+			def update
+				instituicao = Instituicao.find(params[:id])
+				if instituicao.update_attributes(instituicao_params)
+					render json: {status: 'SUCCESS', message:'Instituicao atualizada', data:instituicao},status: :ok
+				else
+					render json: {status: 'ERROR', message:'Instituicao nao atualizada', data:instituicao.erros},status: :unprocessable_entity
+				end
+			end
+			# Parametros aceitos
+			private
+			def instituicao_params
+				params.permit(:nome, :cnpj, :tipo)
+			end
+    end
+	end
+end
